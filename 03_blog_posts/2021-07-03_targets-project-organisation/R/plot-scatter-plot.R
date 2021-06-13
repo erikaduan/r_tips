dcbeceff6e31f94027336f25e53a3f12b68a04ea
erikaduan <34994264+erikaduan@users.noreply.github.com>
2021-06-13 22:46:21 +1000

    update file names

---
+++ 03_blog_posts/2021-07-03_targets-project-organisation/R/plot-scatter-plot.R
@@ -0,0 +1,13 @@
+#' 01_plot-scatter-plot  
+#'
+#' @author Erika Duan
+#' @param 
+#' @param 
+#' @param 
+#' @return A scatter plot of y against x styled using ggplot2
+#' @examples
+plot_scatter <- function(data, y, x) {
+  if (!require("tidyverse")) install.packages("tidyverse")
+  
+    
+}
